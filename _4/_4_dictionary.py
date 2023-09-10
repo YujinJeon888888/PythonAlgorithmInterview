84e9962b0a54592bacbf7b51ce4d06be59771b88
YujinJeon888888 <yujinjun@ewhain.net>
2023-09-10 22:02:42 +0900

    add: 5장

---
+++ _4/_4_dictionary.py
@@ -0,0 +1,29 @@
+﻿person= {'name': 'Alice','age': 30,'city': 'New York'}
+print("name: ",person['name'])
+print("Age: ",person['age'])
+print("City: ",person['city'])
+#딕셔너리 값 변경
+person['age']=31
+
+print("Updated Age: ",person['age'])
+
+#딕셔너리에 새로운 키, 값 추가
+person['job']='Engineer'
+
+print("job: ",person['job'])
+
+#딕셔너리 순회
+for key, value in person.items():
+    print(f"{key}: {value}")
+
+#딕셔너리 요소 삭제
+del person['city']
+
+print(person)
+
+#딕셔너리에 특정 키 있나 확인
+if 'name' in person:
+    print("Name is in the dictionary")
+else: 
+    print("Name isn't in the dictionary")
+
---
+++ _4/_4_dictionary_module.py
@@ -0,0 +1,28 @@
+﻿from collections import defaultdict
+from collections import Counter
+import collections
+
+fruit_count=defaultdict(int)#int 타입의 기본값 가지는 defaultdict생성
+
+#딕셔너리에 값 추가
+fruit_count['apple']+=1
+fruit_count['banana']+=2
+fruit_count['cherry']+=3
+
+print(fruit_count['apple'])   # 출력: 1
+print(fruit_count['banana'])  # 출력: 2
+print(fruit_count['cherry'])  # 출력: 3
+print(fruit_count['grape'])   # 출력: 0 (기본값이 0이므로)
+
+
+#counter
+print("counter  ↓")
+a = [1,2,3,4,5,5,5,6,6]
+
+b = collections.Counter(a)
+print(b)
+### result Counter({5:3, 6:2, 1:1, 2:1, 3:1, 4:1})
+
+##### 가장 빈도가 높은 것은?
+b.most_common(1)
+[(5,3)]
